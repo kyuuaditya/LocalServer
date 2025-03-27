fb3c577d4a02541dde06042b23e648c15fc0db0e
Aditya Pratap Singh <thisisaditya.me@gmail.com>
2025-03-27 10:41:37 +0530

    changed common ipv4 to wifi only

--- ip_fetch.py
+++ ip_fetch.py
@@ -1,9 +1,19 @@
 import socket
+import psutil
 import requests
 import credentials
 
-hostname = socket.gethostname()
-local_ip = socket.gethostbyname(hostname)
+# hostname = socket.gethostname()
+# local_ip = socket.gethostbyname(hostname)
+
+def get_ipv4_address():
+    for interface, addrs in psutil.net_if_addrs().items():
+        for addr in addrs:
+            if addr.family == socket.AF_INET and "Wi-Fi" in interface:  # Adjust for your system
+                return addr.address
+    return "No Wi-Fi IPv4 found"
+
+local_ip = get_ipv4_address()
 
 message = f"🔹 Your Local Server link: {local_ip}:8008"
 url = f"https://api.telegram.org/bot{credentials.BOT_TOKEN}/sendMessage?chat_id={credentials.CHAT_ID}&text={message}"
